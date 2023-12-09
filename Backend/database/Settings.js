2d1b73442408607b27dfd3d4c4df76b999698bc4
mariammahrous23 <71783484+mariammahrous23@users.noreply.github.com>
2023-12-09 18:17:52 +0200

    Backend

---
+++ Backend/database/Settings.js
@@ -0,0 +1,56 @@
+import express from "express";
+import { db } from "./connection.js";
+import bodyParser from 'body-parser';
+
+const Update_Pass = express.Router();
+const dbInstance = await db();
+
+
+// Middleware to parse incoming requests
+Update_Pass.use(bodyParser.json());
+
+// Update_pass API
+Update_Pass.get('/api/UpdatePassword/', async (req, res) => {
+    const { email, newPassword } = req.body;
+
+    try {
+        const result = await dbInstance.query('UPDATE student  SET passw = $1 WHERE email = $2', [newPassword, email]);
+    
+        if (result.rowCount > 0) {
+            res.json({ success: true, message: 'Password updated successfully' });
+        } else {
+          res.status(404).json({ success: false, message: 'User not found or password not updated' });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+      }
+  
+});
+
+/*Update_Pass.get('/api/UpdatePassword/', async (req, res) => {
+    const { email, password: newPassword } = req.query;
+    
+    console.log('Updating password for email:', email, 'with new password:', newPassword);
+
+    try {
+        const result = await dbInstance.query('UPDATE student SET passw = $1 WHERE email = $2', [newPassword, email]);
+        console.log('Query result:', result);
+
+        if (result.rowCount > 0) {
+            res.json({ success: true, message: 'Password updated successfully' });
+        } else {
+            res.status(404).json({ success: false, message: 'User not found or password not updated' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+});
+
+
+*/
+
+export { Update_Pass };
+
--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
-import express from "express";
+import express from 'express';
 import posts from './routes/posts.js';
 import bodyParser from "body-parser";
 import cors from 'cors';
+
+import { Update_Pass } from './database/Settings.js';
+import { db } from './database/connection.js';
 
 const port = 8080;
 const app=express();
@@ -17,9 +20,19 @@ app.use(
 );
 
 
-app.use('/api/', posts);
+// Use async/await to wait for the database connection before starting the server
+(async () => {
+  try {
+    await db(); // Call the db function to establish the database connection
+    app.use('/', posts);
+    app.use('/', Update_Pass);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-  
+    
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to establish database connection:', error);
+  }
+})();
