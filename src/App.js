20056a49635003eb409b41936d3d62c4b38e405d
mariammahrous23 <71783484+mariammahrous23@users.noreply.github.com>
2023-12-09 19:15:21 +0200

    Sidebar clicks bt link to other pages (no json till u say so )

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StudentDashboard from './pages/StudentDashboard.js';
 import StudentSetting from './pages/StudentSettings.js';
-
 
 function App() {
   return (
     <>
       <div className='App'>
-        <StudentDashboard />
+        <Router>
+          <Routes>
+            <Route path="/" element={<StudentDashboard />} />
+            <Route path="/Settings" element={<StudentSetting />} />
+          </Routes>
+        </Router>
       </div>
       <div className='Mobile'>
         <h2>
