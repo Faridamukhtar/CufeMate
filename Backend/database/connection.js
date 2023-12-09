29ac587509c6036f57ec81924fe92ba1ea49dde7
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2023-12-09 13:55:10 +0200

    new main

--- Backend/database/connection.js
+++ Backend/database/connection.js
@@ -4,7 +4,17 @@ import pgInstance from 'pg';
 export const db = async () =>
 {
   const {Client} =pgInstance;
+<<<<<<< Updated upstream
   const client = new Client(process.env.DATABASE_URL);
+=======
+  //const client = new Client(process.env.DATABASE_URL);
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+  });
+>>>>>>> Stashed changes
   client.connect((err) => {
     if (err) {
       console.error('Error connecting to the database:', err.stack);
