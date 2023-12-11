2fdc86ef200b9d3039bf1de91731925f7d376c2a
Chahd ElZalaki <shahd.zalaky@gmail.com>
2023-12-11 18:35:02 +0200

    lessa

---
+++ Backend/database/Contacts.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { db } from "./connection.js";
+
+const reps_contacts_router=express.Router();
+const dbInstance = await db();
+
+reps_contacts_router.get("/api/repscontacts/", async (req,res) =>{
+    //const {std_id}=req.params;
+    //const {std_major}=req.params;
+    const major='CCEC';
+    const classs=2025;
+    try 
+    {
+        const result =await dbInstance.query ("SELECT fname, lname, email FROM student WHERE major_id=$1 AND class=$2 AND rep_flag=1;",[major,classs]);
+        res.json(result.rows);
+    }
+    catch (error)
+    {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+export { reps_contacts_router};
--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,29 @@
 import express from "express";
+//import posts from './routes/posts.js';
+import bodyParser from "body-parser";
+import cors from 'cors';
+//import { previous_complaints_router } from './database/PreviousComplaints.js';
+//import { write_complaint_router } from './database/WriteComplaint.js';
+import { reps_contacts_router } from './database/Contacts.js';
 
+const port = 8080;
 const app=express();
 
-app.use(express.json);
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+
+app.use('/', reps_contacts_router );
+//app.use('/', previous_complaints_router);
+//app.use('/', write_complaint_router);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
