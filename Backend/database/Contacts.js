859cabf03fbad49fb27ec522cb10c665e661585c
Chahd ElZalaki <shahd.zalaky@gmail.com>
2023-12-13 20:53:58 +0200

    bla

--- Backend/database/Contacts.js
+++ Backend/database/Contacts.js
@@ -4,14 +4,15 @@ import { db } from "./connection.js";
 const reps_contacts_router=express.Router();
 const dbInstance = await db();
 
-reps_contacts_router.get("/api/repscontacts/:major/:class", async (req,res) =>{
+reps_contacts_router.get("/api/repscontacts/:major/:class", async (req,res) =>
+{
     //const {std_id}=req.params;
     //const {std_major}=req.params;
     try 
     {
         const major='CCEC';
         const classs=2025;
-        const result =await dbInstance.query ("SELECT fname, lname, email FROM student WHERE major_id=$1 AND class=$2 AND rep_flag= B'1';",[major,classs]);
+        const result =await dbInstance.query ("SELECT fname, lname, email FROM student WHERE major_id=$1 AND class=$2 AND rep_flag= 1;",[major,classs]);
         res.json(result.rows);
     }
     catch (error)
