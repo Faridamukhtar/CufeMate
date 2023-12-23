88fdf2bed6f472bf49d56096976747035527b704
Chahd ElZalaki <shahd.zalaky@gmail.com>
2023-12-23 22:42:20 +0200

    stored proc

--- Backend/database/GetAdminpass.js
+++ Backend/database/GetAdminpass.js
@@ -11,7 +11,7 @@ ChangeAdminPass_router.get('/api/GetAdminsPass', async (req, res) => {
     const {id} = req.query;
 
   try {
-    const result = await dbInstance.query('SELECT passw FROM admins WHERE admin_id= $1', [id]);
+    const result = await dbInstance.query('SELECT get_admin_pass($1) AS passw;', [id]);
     res.json(result.rows);
   } catch (error) {
     console.error(error);
