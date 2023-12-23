045793720c04ef0c7be2e08d0cf38518ce684033
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2023-12-24 00:32:22 +0200

    edits

--- src/pages/ReqToWritePostDashboard.js
+++ src/pages/ReqToWritePostDashboard.js
@@ -2,16 +2,20 @@ import React from "react";
 import Sidebar from "../Components/Sidebar";
 import Dashboard from "../Components/Dashboard";
 import './ReqToWritePostDashboard.css'
+import { useParams } from "react-router-dom";
 
 const ReqToWritePostDashboard =()=>
 {
+    let student={std_id:0, fname:'', lname:'', class:2026}
+    student = useParams();
+
     return (
         <div className="ReqWritePostPage">
             <div className="sidebar">
-                <Sidebar SidebarType='student'/>
+                <Sidebar SidebarType='student' studentData={student}/>
             </div>
             <div className="ReqWriteDashboard">
-                <Dashboard DashboardType='reqwritepost'/>
+                <Dashboard DashboardType='reqwritepost' studentData={student}/>
             </div>
         </div>
     );
