50fe2db958f12bf2b417f3bf57ab3d1ff4e180d3
Chahd ElZalaki <shahd.zalaky@gmail.com>
2023-12-27 22:19:31 +0200

    edit nono

--- src/Components/PreviousComplaints.js
+++ src/Components/PreviousComplaints.js
@@ -29,9 +29,9 @@ const getComplaints = async (std_id,title="",complaint_date="", stat="") =>
 
 function Complaint(props)
 {
-  const ChooseBoxColor= props.stat==='0'? 'PendingRect':'ReadRect';
-  const ChooseBoxTextColor=props.stat==='0'? 'PendingText':'ReadText';
-  const StatusText=props.stat==='0'? 'Pending':'Read';
+  const ChooseBoxColor= props.stat===0? 'PendingRect':'ReadRect';
+  const ChooseBoxTextColor=props.stat===0? 'PendingText':'ReadText';
+  const StatusText=props.stat===0? 'Pending':'Read';
    return(
         <div className="ComplaintRect">
               <div className={ChooseBoxColor}>
